df486131bdb1af7418146a2edc7c665f96ae620f
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2023-12-29 21:49:06 -0300

    :sparkles: feat: server routes & web design

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,13 @@
 // Back-end API RESTful
 
-import Fastify from "fastify";
-import cors from '@fastify/cors';
-import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify"
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-    
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
