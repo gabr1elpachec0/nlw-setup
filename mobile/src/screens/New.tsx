76197aa9c5593452370c24b63b831fc9c899245b
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-01-03 13:57:18 -0300

    feat: local notification & mobile screens

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,22 +1,17 @@
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { BackButton } from "../components/BackButton";
-import { Checkbox } from "../components/Checkbox";
 import { useState } from "react";
-import { Feather } from '@expo/vector-icons';
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
 
-const availableWeekDays = [
-  'Domingo',
-  'Segunda-feira',
-  'Terça-feira',
-  'Quarta-feira',
-  'Quinta-feira',
-  'Sexta-feira',
-  'Sábado'
-]
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { api } from "../lib/axios";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
@@ -26,13 +21,30 @@ export function New() {
     }
   }
 
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      }
+
+      await api.post('/habits', { title, weekDays })
+      
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView 
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
-
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -40,44 +52,47 @@ export function New() {
         </Text>
 
         <Text className="mt-6 text-white font-semibold text-base">
-          Qual seu comprometimento? 
+          Qual seu comprometimento?
         </Text>
 
         <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
-          placeholder="Exercícios, dormir bem, etc."
+          placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
-        <Text className="mt-4 mb-3 text-white font-semibold text-base">
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
         </Text>
 
         {
-          availableWeekDays.map((weekDay, i) => (
-            <Checkbox 
+          availableWeekDays.map((weekDay, index) => (
+            <Checkbox
               key={weekDay}
               title={weekDay}
-              checked={weekDays.includes(i)}
-              onPress={() => handleToggleWeekDay(i)}
+              checked={weekDays.includes(index)}
+              onPress={() => handleToggleWeekDay(index)}
             />
           ))
         }
-        
+
         <TouchableOpacity
-          className="flex-row w-full h-14 items-center justify-center bg-green-600 rounded-lg mt-6"
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather 
-            name="check"
-            size={20}
-            color={colors.white}
+           name="check"
+           size={20}
+           color={colors.white}
           />
 
           <Text className="font-semibold text-base text-white ml-2">
             Confirmar
           </Text>
         </TouchableOpacity>
-
       </ScrollView>
     </View>
   )
